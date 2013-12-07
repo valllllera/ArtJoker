aac557cf6b3f6dccda2dbb6e559e0d88b2a15b60
evgeniytka4enko <evgeniytka4enko@gmail.com>
2013-12-07 14:30:28 +0200

    init commit

---
+++ ArtJoker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArtJoker
+//
+//  Created by Evgeniy Tka4enko on 07.12.13.
+//  Copyright (c) 2013 vexadev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
